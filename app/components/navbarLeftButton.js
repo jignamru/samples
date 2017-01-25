9c41f357973dbe990b414b127dda3b98167b9592
Jigna <jigna@lamorisyenne.com>
2017-01-24 20:35:04 -0800

    fixing 'useNativeDriver' warning

--- app/components/navbarLeftButton.js
+++ app/components/navbarLeftButton.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import ReactNative from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+var common = require('../common/styles');
 
 const {
   Component,
 } = React;
 
 const {
-  Image,
-  TouchableOpacity,
+  View,
+  TouchableHighlight,
 } = ReactNative;
 
 export default class BackArrow extends Component {
   render() {
     return (
-      <TouchableOpacity onPress={this.props.onPress}>
-        <Image
-          source={require('../images/icons/left-arrow.png')}
-          style={[{ width: 30, height: 30, marginLeft: 5, marginTop:10 }, this.props.style]}/>
-      </TouchableOpacity>
+      <TouchableHighlight
+        onPress={this.props.onPress}>
+          <View>
+            <Icon name='angle-left' size={30} style={{marginLeft: 5, marginTop:10, color: common.color.grey}}/>
+          </View>
+      </TouchableHighlight>
     );
   }
 }
