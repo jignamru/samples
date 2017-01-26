f07759de74bc1ad32f5d9ec6b67a66aed511848b
Jigna <jigna@lamorisyenne.com>
2017-01-26 12:51:25 -0800

    updating regex to make the '+' optional

--- app/common/formFieldValidators.js
+++ app/common/formFieldValidators.js
@@ -35,7 +35,7 @@ module.exports = {
 	validatePhone: function(value) {
 	    if(!value || value == '') return "Mobile number is required.";
 
-		var re = /^(\+[1])?(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$/;
+		var re = /^(?:(\+)?[1])?(?:\([2-9]\d{2}\)\ ?|[2-9]\d{2}(?:\-?|\ ?))[2-9]\d{2}[- ]?\d{4}$/;
 		
 		var matches = re.test(value);
 	    if (!matches) return "Please enter a valid US phone number.";
