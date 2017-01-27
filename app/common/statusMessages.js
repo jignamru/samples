96c2dbd845e0b8c8abe35c13aedac269007be124
Jigna <jigna@lamorisyenne.com>
2017-01-26 20:36:11 -0800

    text updates

--- app/common/statusMessages.js
+++ app/common/statusMessages.js
@@ -5,11 +5,11 @@ module.exports = {
 		status: {
 			COULD_NOT_FIND_SITTER: "None of your sitters are available",
 			IN_PROGRESS: "We're working on it...",
-			BOOKED: "You got a sitter! Woohoo!"
+			BOOKED: "Woohoo!"
 		},
 		type: {
-			NORMAL: 'Non urgent request (We sent a request to your sitters by priority)',
-			URGENT: 'Urgent request (We sent a request to *all* your sitters)'
+			NORMAL: 'Non urgent (We sent a request to your sitters by priority)',
+			URGENT: 'Urgent (We sent a request to *all* your sitters)'
 		}
 	}
 };
--- app/components/termsAndConditionsModal.js
+++ app/components/termsAndConditionsModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import CustomText from '../components/customText';
 import CustomButton from '../components/customButton';
 import CustomModal from '../components/customModal';
@@ -21,17 +21,35 @@ export default class TermsAndConditions extends Component {
 
   render() {
     return (
-        <CustomModal modalTitle="Terms & Conditions" hideButton={true} visible={this.state.modalVisible}>
+        <CustomModal modalTitle='Terms & Conditions ("Terms")' hideButton={true} visible={this.state.modalVisible}>
           <ScrollView>
               <CustomText style={styles.container}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consectetur quis sem nec tincidunt. Vestibulum porttitor libero nec ipsum porta molestie. Maecenas purus augue, eleifend luctus nisi ut, auctor congue purus. Pellentesque quis arcu quis libero bibendum placerat. Etiam pharetra viverra risus, eget sodales mi ornare ac. Nunc eu nibh tellus. Nullam id faucibus erat. Nulla magna mauris, varius et maximus vitae, venenatis sit amet justo. Pellentesque a nisi tempor, commodo magna et, interdum nibh. Quisque non varius neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-      Quisque sodales eleifend massa, non dignissim justo condimentum sit amet. Cras quis gravida dolor. Fusce id consequat felis. Proin condimentum ligula eros. Donec eu posuere turpis. In eget urna et mi pretium mattis nec vel tortor. Integer sagittis ac libero nec cursus. Cras dapibus congue tortor, sit amet volutpat lorem efficitur id. Praesent condimentum egestas magna a egestas. Nunc commodo pellentesque quam, in ultricies massa varius eu. Mauris vel mauris ac neque commodo mollis.
-
-      Duis cursus leo ante, nec scelerisque ante euismod id. Cras fermentum ultricies orci at venenatis. Morbi consequat ultrices urna sed congue. Suspendisse potenti. Nunc in lacus eu libero gravida porta nec vitae nunc. Nam ut tincidunt augue. Aenean lacinia erat sit amet orci scelerisque dictum. Sed vel arcu id purus consectetur sagittis. Duis aliquam laoreet neque ut pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec tempor quis arcu blandit varius. Duis ac erat varius, elementum eros scelerisque, lacinia erat. Nullam et velit venenatis risus finibus scelerisque sit amet in turpis.
-
-      Praesent tincidunt sed dolor at iaculis. Aenean elementum, dolor nec varius pulvinar, nibh risus semper magna, sit amet faucibus mi massa eu eros. Nam mollis sem magna, id ornare sem congue sed. Curabitur vel odio justo. Fusce porttitor iaculis diam ut ornare. Mauris ut dui ligula. Sed pulvinar mattis cursus. Curabitur volutpat convallis magna, eget hendrerit sapien imperdiet sit amet. Maecenas vestibulum ante at urna scelerisque, nec pharetra orci bibendum. Duis aliquet ante eu euismod condimentum.
-
+Last updated: Jan 24th, 2017
+{'\n\n'}
+Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the Sitter Done mobile application (the "Service") operated by Karmagiri LLC ("us", "we", or “our").
+{'\n\n'}
+Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
+{'\n\n'}
+By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.
+{'\n\n'}
+Subscriptions
+{'\n\n'}
+Some parts of the Service are billed on a subscription basis ("Subscription(s)"). You will be billed in
+advance on a recurring ...
+{'\n\n'}
+Content
+{'\n\n'}
+Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the …
+{'\n\n'}
+Links To Other Web Sites
+{'\n\n'}
+Our Service may contain links to third-party web sites or services that are not owned or controlled by Karmagiri LLC.
+{'\n\n'}
+Karmagiri LLC has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Karmagiri LLC shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.
+{'\n\n'}
+Changes
+{'\n\n'}
+We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days\' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
               </CustomText>
               <CustomButton
                 onPress={this.toggleModal.bind(this)}
